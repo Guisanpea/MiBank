8a329328b5d23045250095cf48131f4e206e4e99
Santi <guisanpea@gmail.com>
2018-06-04 17:27:21 +0200

    Starting

---
+++ build/libs/exploded/MiBank-0.0.1-SNAPSHOT.ear/war-0.0.1-SNAPSHOT.war/index.jsp
@@ -0,0 +1,16 @@
+<%@ page import="entities.Account" %><%--
+  Created by IntelliJ IDEA.
+  User: ubuntie
+  Date: 3/06/18
+  Time: 19:46
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <%=  %>
+  </body>
+</html>
---
+++ ejb/src/main/java/ejb/AccountFacade.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import entities.Account;
+import entities.User;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author ubuntie
+ */
+@Stateless
+public class AccountFacade extends AbstractFacade<Account> {
+
+    @PersistenceContext(unitName = "MiBank-ejbPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public AccountFacade() {
+        super(Account.class);
+    }
+    /*
+    public Account findByAccountNumber(String number){
+        int bank = Integer.valueOf(number.substring(0,4));
+        int office = Integer.valueOf(number.substring(4,8));
+        int control = Integer.valueOf(number.substring(8,10));
+        int id = Integer.valueOf(number.substring(10,20));
+        
+        Query query = em.createNamedQuery("Account.findByFullNumber");
+        query.setParameter("bank", bank);
+        query.setParameter("office", office);
+        query.setParameter("control", control);
+        query.setParameter("id", id);
+        
+        return (Account) query.getSingleResult();
+    }*/
+    
+    public Account findByUser(User user){
+        Query query = em.createNamedQuery("Account.findByUser");
+        query.setParameter("user", user);
+        
+        return (Account) query.getSingleResult();
+    }
+}
